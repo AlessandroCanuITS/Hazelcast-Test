9f090c44216e4e438507a874a3f4957ddd48ed33
Alessandro Canu <alecan448@gmail.com>
2022-09-15 13:45:48 +0000

    added sink

---
+++ terraform-gcp/cloud_run_sink.tf
@@ -0,0 +1,17 @@
+resource "google_cloud_run_service" "default" {
+  name     = "cloudrun-hello-tf"
+  location = var.region
+
+  template {
+    spec {
+      containers {
+        image = "gcr.io/cloudrun/hello"
+      }
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
